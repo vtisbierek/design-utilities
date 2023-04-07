da2389e251f4180836f341a9b875c6b4279b351b
Victor Tisbierek <metropoller@gmail.com>
2023-04-07 11:33:22 -0300

    including kmong

--- src/components/SocialApps.tsx
+++ src/components/SocialApps.tsx
@@ -1,13 +1,20 @@
 import styles from "../styles/socialApps.module.scss";
 import Image from "next/image";
 import kakaoIcon from "../../public/kakao_icon.png";
+import kmongIcon from "../../public/kmong_icon.png";
 
 export default function SocialApps(){
     return (
         <div className={styles.container}>
-            <a href="https://center-pf.kakao.com/_Zgmxixj/dashboard" target="_blank">
-                <Image src={kakaoIcon} alt="kakao icon"/>
-            </a>
+            <h1>Customer Chats</h1>
+            <div className={styles.logoArea}>
+                <a href="https://center-pf.kakao.com/_Zgmxixj/chats" target="_blank">
+                    <Image src={kakaoIcon} alt="kakao icon"/>
+                </a>
+                <a href="https://kmong.com/inboxes?page=1&type=&keyword=" target="_blank">
+                    <Image src={kmongIcon} alt="kmong icon"/>
+                </a>
+            </div>
         </div>
     );
 }
