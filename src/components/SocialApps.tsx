384e675d33feced72f6e103e2a160fb6dbe6f241
Victor Tisbierek <metropoller@gmail.com>
2023-04-07 12:14:08 -0300

    including social media

--- src/components/SocialApps.tsx
+++ src/components/SocialApps.tsx
@@ -1,20 +1,36 @@
 import styles from "../styles/socialApps.module.scss";
 import Image from "next/image";
-import kakaoIcon from "../../public/kakao_icon.png";
-import kmongIcon from "../../public/kmong_icon.png";
+import {StaticImageData} from "next/image";
 
-export default function SocialApps(){
+type Social = {
+    href: string;
+    src: StaticImageData;
+    alt: string;
+}
+
+interface SocialProps{
+    title: string;
+    socials: Social[];
+}
+
+export default function SocialApps({title, socials}: SocialProps){
     return (
         <div className={styles.container}>
-            <h1>Customer Chats</h1>
+            <h1>{title}</h1>
             <div className={styles.logoArea}>
-                <a href="https://center-pf.kakao.com/_Zgmxixj/chats" target="_blank">
-                    <Image src={kakaoIcon} alt="kakao icon"/>
-                </a>
-                <a href="https://kmong.com/inboxes?page=1&type=&keyword=" target="_blank">
-                    <Image src={kmongIcon} alt="kmong icon"/>
-                </a>
+                {socials.map(entry => (
+                    <a href={entry.href} target="_blank" key={entry.alt}>
+                        <Image src={entry.src} alt={entry.alt}/>
+                    </a>
+                ))}
             </div>
         </div>
     );
 }
+
+{/*<a href="https://center-pf.kakao.com/_Zgmxixj/chats" target="_blank">
+    <Image src={kakaoIcon} alt="kakao icon"/>
+</a>
+<a href="https://kmong.com/inboxes?page=1&type=&keyword=" target="_blank">
+    <Image src={kmongIcon} alt="kmong icon"/>
+</a>*/}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,56 @@ import styles from '@/styles/Home.module.scss';
 import ImageGenerator from '@/components/ImageGenerator';
 import Translator from '@/components/Translator';
 import SocialApps from '@/components/SocialApps';
+import kakaoIcon from "../../public/kakao_icon.png";
+import kmongIcon from "../../public/kmong_icon.png";
+import instagramIcon from "../../public/instagram_icon.png";
+import whatsappIcon from "../../public/whatsapp_icon.png";
+import pinterestIcon from "../../public/pinterest_icon.png";
+import behanceIcon from "../../public/behance_icon.png";
 
 export default function Home() {
+  const customerChat = {
+    title: "Customer Chat",
+    socials: [
+      {
+        href: "https://center-pf.kakao.com/_Zgmxixj/chats",
+        src: kakaoIcon,
+        alt: "kakao"
+      },
+      {
+        href: "https://kmong.com/inboxes?page=1&type=&keyword=",
+        src: kmongIcon,
+        alt: "kmong"
+      }
+    ]
+  }
+
+  const socialMedia = {
+    title: "Social Media",
+    socials: [
+      {
+        href: "https://www.instagram.com/untitled.studio.official/",
+        src: instagramIcon,
+        alt: "instagram"
+      },
+      {
+        href: "https://web.whatsapp.com",
+        src: whatsappIcon,
+        alt: "whatsapp"
+      },
+      {
+        href: "https://www.pinterest.co.kr/untitledstudioofficial/",
+        src: pinterestIcon,
+        alt: "pinterest"
+      },
+      {
+        href: "https://www.behance.net/untitled__studio",
+        src: behanceIcon,
+        alt: "behance"
+      },
+    ]
+  }
+
   return (
     <>
       <Head>
@@ -14,7 +62,8 @@ export default function Home() {
       <main className={styles.container}>
         <div className={styles.panelLeft}>
           <Translator />
-          <SocialApps />
+          <SocialApps title={customerChat.title} socials={customerChat.socials}/>
+          <SocialApps title={socialMedia.title} socials={socialMedia.socials}/>
         </div>
         <div className={styles.centerLine}></div>
         <div className={styles.panelRight}>
